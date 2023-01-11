4db52e33dc51b7c6d4d37374a4e510bf0a791091
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-11 14:38:06 -0800

    Create security.js
    [IN PROGRESS] JS file for the main security page.

---
+++ security.js
@@ -0,0 +1,16 @@
+var alphaNumeric = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","0"];
+
+function getChar(){
+var randChar = alphaNumeric[Math.floor(Math.random() * alphaNumeric.length)];
+return randChar;
+}
+
+function genCode() {
+  var codeString = "";
+  for (let i=0; i<20; i++) {
+  codeString += getChar();
+  }
+  return codeString;
+}
+
+var passcode = genCode();
