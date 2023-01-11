64fb1c6548a8c1510ebf2b6925c5465bdaa715ef
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-11 14:56:11 -0800

    Update security.js

--- security.js
+++ security.js
@@ -1,6 +1,6 @@
 var alphaNumeric = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","0"];
 
-function getChar(){
+function getChar() {
 var randChar = alphaNumeric[Math.floor(Math.random() * alphaNumeric.length)];
 return randChar;
 }
@@ -15,7 +15,7 @@ function genCode() {
 
 var passcode = genCode();
 
-function checkCode {
+function checkCode() {
   var guessCode = prompt('Paste in the code:');
   if (guessCode == passcode) { 
     alert('You\'re in.'); 
