6096f75843401de991d6b4ac52d9cf3fe5de581f
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-12 16:37:56 -0800

    [IN PROGRESS] main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function getSelectVal() {
+  var cloakOps = document.getElementbyId("cloakOps");
+  var titleElem = document.getElementById("titleElem");
+  var fav = document.getElementById("fav")
+  if (cloakOps.value === "clever") {
+      fav.setAttribute("src", "clever-fav.ico");
+      titleElem.innerHTML = "Clever | Portal";
+  } else if (cloakOps.value === "schoology") {
+    fav.setAttribute("src", "schoology-fav.ico");
+    titleElem.innerHTML = "Home | Schoology";
+  }
+}
