a7e876c3576d8205085c734c26836d0f7135ba70
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-20 15:43:45 -0800

    Update cleverCloak.js

--- cleverCloak.js
+++ cleverCloak.js
@@ -1,7 +1,7 @@
 function cloak() {
   var fav = document.querySelector("link[type='image/x-icon']");
   var titleElem = document.querySelector("title");
-  fav.setAttribute("href", "https://tumbleweed64.github.io/clever-fav.ico");
+  fav.setAttribute("href", "https://tumbleweed-64.github.io/clever-fav.ico");
   titleElem.innerHTML = "Clever | Portal";
 }
 
