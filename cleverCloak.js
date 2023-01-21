ed11d5ded9af0dc62e2e5ed4c32d80df116679a2
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-20 16:02:46 -0800

    Delete cleverCloak.js

--- cleverCloak.js
+++
@@ -1,8 +0,0 @@
-function cloak() {
-  var fav = document.querySelector("link[type='image/x-icon']");
-  var titleElem = document.querySelector("title");
-  fav.setAttribute("href", "https://tumbleweed-64.github.io/clever-fav.ico");
-  titleElem.innerHTML = "Clever | Portal";
-}
-
-cloak();
