0fb2324e15d61fad4492e6b1ed81e51a4ea59bc9
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-27 16:58:32 -0800

    Update clicker_scripts.js

--- clicker_scripts.js
+++ clicker_scripts.js
@@ -1,17 +1,5 @@
 var cps = 0;
 
-function genClickFrame() {
-  for (var i = 0; i < 5; i++) {
-    document.getElementById("clickFrame").appendChild("br");
-  }
-  var clickBtn = document.createElement("img");
-  clickBtn.src = "button.png";
-  clickBtn.setAttribute("id", "clickBtn");
-  clickBtn.setAttribute("onmousedown", "buttonShrink();");
-  clickBtn.setAttribute("onmouseup", "buttonGrow();");
-  document.getElementById("clickFrame").appendChild(clickBtn);
-}
-
 function buttonShrink() {
   var clickBtn = document.getElementById("clickBtn");
   clickBtn.width -= clickBtn.width * 0.5;
