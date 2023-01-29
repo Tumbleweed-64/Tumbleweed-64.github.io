411822aacb50a8715c47902b91aba1ef67372ccb
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-28 17:28:13 -0800

    Update clicker_scripts.js

--- clicker_scripts.js
+++ clicker_scripts.js
@@ -2,14 +2,14 @@ var cps = 0;
 
 function buttonShrink() {
   var clickBtn = document.getElementById("clickBtn");
-  clickBtn.width -= clickBtn.width * 0.5;
-  clickBtn.height -= clickBtn.height * 0.5;
+  clickBtn.width -= (clickBtn.style.width * 0.5);
+  clickBtn.height -= (clickBtn.style.height * 0.5);
 }
 
 function buttonGrow() {
   var clickBtn = document.getElementById("clickBtn");
-  clickBtn.style.width += clickBtn.style.width * 0.5;
-  clickBtn.style.height += clickBtn.style.height * 0.5;
+  clickBtn.style.width += (clickBtn.style.width * 0.5);
+  clickBtn.style.height += (clickBtn.style.height * 0.5);
 }
 
 function handleClick() {
@@ -17,6 +17,6 @@ function handleClick() {
   lvlProgress.value += 1;
   if (lvlProgress.value === lvlProgress.max) {
     lvlProgress.value = "0";
-    lvlProgress.max += Math.floor(Math.random() * 25) + 1
+    lvlProgress.max += Math.floor(Math.random() * 25) + 1;
   }
 }
