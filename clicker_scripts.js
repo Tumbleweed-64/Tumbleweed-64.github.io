b845a2441874454d5e3fef2b41b2c38f49ff0f6e
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-01-30 15:50:27 -0800

    Update clicker_scripts.js

--- clicker_scripts.js
+++ clicker_scripts.js
@@ -1,10 +1,20 @@
 var cps = 0;
+var shopPoints = 0;
 
 function handleClick() {
   var lvlProgress = document.getElementById("lvlProgress");
   lvlProgress.value += 1;
   if (lvlProgress.value === lvlProgress.max) {
+    shopPoints++;
     lvlProgress.value = "0";
     lvlProgress.max += Math.floor(Math.random() * 25) + 1;
+  }
+}
+
+function getUpgrade(upgrade, cost) {
+  document.getElementById(upgrade);
+  if ((upgrade.id == "autoclick1") && (shopPoints == cost)) {
+    cps++;
+    cost++;
   }
 }
